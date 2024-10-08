8239e0a3ff315b021b2b51e7d3bbd14cefd9a67e
suprince <suprince.shakya@swivt.com.np>
2024-10-08 13:34:23 +0545

    added email configurations

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -22,7 +22,8 @@ app.use(express.static(publicPath));
 // Registering Middlewares
 app.use(cors(corsOption));
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ limit: '2mb', extended: true }));
 app.use(errorHandling);
 
 // Defining routes
--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema(
 		},
 		image: {
 			type: String,
+			default: '',
 			required: false,
 		},
 		contactNumber: {
